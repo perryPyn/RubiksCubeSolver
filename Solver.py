6798786eb58e516575fbe97e2f0500f26faf2a5a
Lucas <lcontal54@gmail.com>
2026-04-27 18:42:05 +0200

    changement de la structure du cube + ajout lecture graphique de séquence

--- Solver.py
+++ Solver.py
@@ -1,214 +1,301 @@
-import re
 import os;os.system("cls" if os.name == "nt" else "clear")
+from time import sleep
+from turtle import *;hideturtle();screensize(500, 600, "black")
 
 solveSequence = ''
 
 index = [0,1,2,7,8,3,6,5,4]
-colors = {'w':"#FFFFFF",'y':"#FFFF00",'g':"#008000",'b':"#0000FF",'o':"#FF8C00",'r':"#FF0000"}
+colors = {"u":"#FFFFFF","d":"#FFFF00","r":"#0000FF","l":"#008000","f":"#FF0000","b":"#FF8C00"}
 
-linkEdges = {"u":{1:('b',5),3:('r',1),5:('f',1),7:('l',1)},
-             "l":{1:('u',1),3:('f',7),5:('d',7),7:('b',7)},
-             "r":{1:('u',3),3:('b',3),5:('d',3),7:('f',3)},
-             "b":{1:('d',5),3:('r',3),5:('u',1),7:('l',7)},
-             "f":{1:('u',5),3:('r',7),5:('d',1),7:('l',3)},
-             "d":{1:('f',5),3:('r',5),5:('b',1),7:('l',5)}}
-linkFaces = {"u":{'b':(1,5),'r':(3,1),'f':(5,1),'l':(7,1)},
-             "l":{'u':(1,1),'f':(3,7),'d':(5,7),'b':(7,7)},
-             "r":{'u':(1,3),'b':(3,3),'d':(5,3),'f':(7,3)},
-             "b":{'d':(1,5),'r':(3,3),'u':(5,1),'l':(7,7)},
-             "f":{'u':(1,5),'r':(3,7),'d':(5,1),'l':(7,3)},
-             "d":{'f':(1,5),'r':(3,5),'b':(5,1),'l':(7,5)}}
-opposedColor = {'b':'g','g':'b','w':'y','y':'w','r':'o','o':'r'}
-linkColorFace = {"w":'u',"g":'l',"b":'r',"o":'b',"r":'f',"y":'d'}
+linkEdges = {"u":{1:("r",5),3:("f",1),5:('f',1),7:('l',1)},
+             "l":{1:('u',1),3:('f',7),5:('d',7),7:("r",7)},
+             "r":{1:('u',3),3:("r",3),5:('d',3),7:('f',3)},
+             "b":{1:('d',5),3:("f",3),5:('u',1),7:('l',7)},
+             "f":{1:('u',5),3:("f",7),5:('d',1),7:('l',3)},
+             "d":{1:('f',5),3:("f",5),5:("r",1),7:('l',5)}}
+linkFaces = {"u":{"r":(1,5),"f":(3,1),'f':(5,1),'l':(7,1)},
+             "l":{'u':(1,1),'f':(3,7),'d':(5,7),"r":(7,7)},
+             "r":{'u':(1,3),"r":(3,3),'d':(5,3),'f':(7,3)},
+             "b":{'d':(1,5),"f":(3,3),'u':(5,1),'l':(7,7)},
+             "f":{'u':(1,5),"f":(3,7),'d':(5,1),'l':(7,3)},
+             "d":{'f':(1,5),"f":(3,5),"r":(5,1),'l':(7,5)}}
+opposedColor = {"r":"l","l":"r","u":"d","d":"u","f":"b","b":"f"}
 
-cube_init = {"u":['w']*9,
-             "l":['g']*9,
-             "r":['b']*9,
-             "b":['o']*9,
-             "f":['r']*9,
-             "d":['y']*9}
-cube = cube_init.copy()
-#for k in cube.keys():
-#    cube[k] = [str(cube_init[k][0])+str(i) for i in range(0,9)]
+cube_o = "uuuuuuuuudddddddddrrrrrrrrrlllllllllfffffffffbbbbbbbbb" # len = 54
+#       |+0      |+9      |+18     |+27     |+36     |+45     
+cube = cube_o
 
-def R(direction :int):
-    if direction == 1:
-        t = cube["u"].copy()
-        for i in [2,3,4]: # Rotation de la tranche
-            cube["u"][i] = cube["f"][i]
-            cube["f"][i] = cube["d"][i]
-            cube["d"][i] = cube["b"][i]
-            cube["b"][i] = t[i]
-            
-        t = cube["r"].copy()
-        for i in range(0,8): # Rotation de la face
-            cube["r"][i%8] = t[(i-2)%8]
-    else :
-        for _ in range(3): R(1)
-def L(direction :int):
-    if direction == 1:
-        t = cube["f"].copy()
-        for i in [0,7,6]:
-            cube["f"][i] = cube["u"][i]
-            cube["u"][i] = cube["b"][i]
-            cube["b"][i] = cube["d"][i]
-            cube["d"][i] = t[i]
-            
-        t = cube["l"].copy()
-        for i in range(0,8):
-            cube["l"][i%8] = t[(i-2)%8]
-    else :
-        for _ in range(3): L(1)
-def U(direction :int):
-    if direction == 1:
-        t = cube["f"].copy()
-        for i in [0,1,2]: # Rotation de la tranche
-            cube["f"][i] = cube["r"][i]
-            cube["r"][i] = cube["b"][i+4]
-            cube["b"][i+4] = cube["l"][i]
-            cube["l"][i] = t[i]
-            
-        t = cube["u"].copy()
-        for i in range(0,8): # Rotation de la face
-            cube["u"][i%8] = t[(i-2)%8]
-    else :
-        for _ in range(3): U(1)
-def D(direction :int):
-    if direction == 1:
-        t = cube["f"].copy()
-        for i in [6,5,4]:
-            cube["f"][i] = cube["l"][i]
-            cube["l"][i] = cube["b"][i-4]
-            cube["b"][i-4] = cube["r"][i]
-            cube["r"][i] = t[i]
-            
-        t = cube["d"].copy()
-        for i in range(0,8):
-            cube["d"][i%8] = t[(i-2)%8]
-    else :
-        for _ in range(3): D(1)
-def F(direction :int):
-    if direction == 1:
-        t = cube["u"].copy()
-        for i in [0,1,2]:
-            cube["u"][i+4]     = cube["l"][i+2]
-            cube["l"][i+2]     = cube["d"][i]
-            cube["d"][i]       = cube["r"][(i-2)%8]
-            cube["r"][(i+6)%8] = t[i+4]
-            
-        t = cube["f"].copy()
-        for i in range(0,8):
-            cube["f"][i%8] = t[(i-2)%8]
-    else :
-        for _ in range(3): F(1)
-def B(direction :int):
-    if direction == 1:
-        t = cube["u"].copy()
-        for i in [0,1,2]:
-            cube["u"][i]       = cube["r"][i+2]
-            cube["r"][i+2]     = cube["d"][i+4]
-            cube["d"][i+4]     = cube["l"][(i-2)%8]
-            cube["l"][(i+6)%8] = t[i]
-            
-        t = cube["b"].copy()
-        for i in range(0,8):
-            cube["b"][i%8] = t[(i-2)%8]
-    else :
-        for _ in range(3): B(1)
+def R():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
 
-#primaryMovement = {"R": R(1),  "L": L(1),  "U": U(1),  "D": D(1),  "F": F(1),  "B": B(1),
-#                   "R'": R(-1),"L'": L(-1),"U'": U(-1),"D'": D(-1),"F'": F(-1),"B'": B(-1)}
-def Sequence(sequence :str):
-    sequence = sequence.upper() # Toutes les lettres sont en majuscule
-    #print(sequence)
-    sequence = sum(([m[0]]*2 if m.endswith('2') else [m] for m in re.findall(r"[A-Z](?:2|')?", sequence)), [])
+    cube[47:50] = cubeCopy[2:5]
+    cube[2:5] = cubeCopy[38:41]
+    cube[38:41] = cubeCopy[11:14]
+    cube[11:14] = cubeCopy[47:50]
+
+    for i in range(18,24): cube[i+2] = cubeCopy[i]
+    cube[18:20] = cubeCopy[24:26]
+
+    cube = ''.join(cube)
+def Rp():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[2:5] = cubeCopy[47:50]
+    cube[38:41] = cubeCopy[2:5]
+    cube[11:14] = cubeCopy[38:41]
+    cube[47:50] = cubeCopy[11:14]
+
+    for i in range(18,24): cube[i] = cubeCopy[i+2]
+    cube[24:26] = cubeCopy[18:20]
+
+    cube = ''.join(cube)
+
+def L():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[6:8] = cubeCopy[51:53]  ;cube[0] = cubeCopy[45]
+    cube[51:53] = cubeCopy[15:17];cube[45] = cubeCopy[9]
+    cube[15:17] = cubeCopy[42:44];cube[9] = cubeCopy[36]
+    cube[42:44] = cubeCopy[6:8]  ;cube[36] = cubeCopy[0]
+
+    for i in range(27,33): cube[i+2] = cubeCopy[i]
+    cube[27:29] = cubeCopy[33:35]
+    cube = ''.join(cube)
+def Lp():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[51:53] = cubeCopy[6:8]  ;cube[45] = cubeCopy[0]
+    cube[15:17] = cubeCopy[51:53];cube[9] = cubeCopy[45]
+    cube[42:44] = cubeCopy[15:17];cube[36] = cubeCopy[9]
+    cube[6:8] = cubeCopy[42:44]  ;cube[0] = cubeCopy[36]
+
+    for i in range(27,33): cube[i] = cubeCopy[i+2]
+    cube[33:35] = cubeCopy[27:29]
+
+    cube = ''.join(cube)
+
+def U():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[36:39] = cubeCopy[18:21]
+    cube[18:21] = cubeCopy[49:52]
+    cube[49:52] = cubeCopy[27:30]
+    cube[27:30] = cubeCopy[36:39]
+
+    for i in range(0,6): cube[i+2] = cubeCopy[i]
+    cube[0:2] = cubeCopy[6:8]
+
+    cube = ''.join(cube)
+def Up():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[18:21] = cubeCopy[36:39]
+    cube[49:52] = cubeCopy[18:21]
+    cube[27:30] = cubeCopy[49:52]
+    cube[36:39] = cubeCopy[27:30]
+
+    for i in range(0,6): cube[i] = cubeCopy[i+2]
+    cube[6:8] = cubeCopy[0:2]
+
+    cube = ''.join(cube)
+
+def D():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[22:25] = cubeCopy[40:43]
+    cube[45:48] = cubeCopy[22:25]
+    cube[31:34] = cubeCopy[45:48]
+    cube[40:43] = cubeCopy[31:34]
+
+    for i in range(9,15): cube[i+2] = cubeCopy[i]
+    cube[9:11] = cubeCopy[15:17]
+
+    cube = ''.join(cube)
+def Dp():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[40:43] = cubeCopy[22:25]
+    cube[22:25] = cubeCopy[45:48]
+    cube[45:48] = cubeCopy[31:34]
+    cube[31:34] = cubeCopy[40:43]
+
+    for i in range(9,15): cube[i] = cubeCopy[i+2]
+    cube[15:17] = cubeCopy[9:11]
+
+    cube = ''.join(cube)
+
+def F():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[4:7] = cubeCopy[29:32]
+    cube[29:32] = cubeCopy[9:12]
+    cube[9:12] = [cubeCopy[24],cubeCopy[25],cubeCopy[18]]
+    cube[24],cube[25],cube[18] = cubeCopy[4],cubeCopy[5],cubeCopy[6]
+
+    for i in range(36,42): cube[i+2] = cubeCopy[i]
+    cube[36:38] = cubeCopy[42:44]
+
+    cube = ''.join(cube)
+def Fp():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[29:32] = cubeCopy[4:7]
+    cube[9:12] = cubeCopy[29:32]
+    cube[24],cube[25],cube[18] = cubeCopy[9],cubeCopy[10],cubeCopy[11]
+    cube[4],cube[5],cube[6] = cubeCopy[24],cubeCopy[25],cubeCopy[18]
+
+    for i in range(36,42): cube[i] = cubeCopy[i+2]
+    cube[42:44] = cubeCopy[36:38]
+
+    cube = ''.join(cube)
+
+def B():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[0:3] = cubeCopy[20:23]
+    cube[20:23] = cubeCopy[13:16]
+    cube[13:16] = cubeCopy[33],cubeCopy[34],cubeCopy[27]
+    cube[33],cube[34],cube[27] = cubeCopy[0:3]
+
+    for i in range(45,51): cube[i+2] = cubeCopy[i]
+    cube[45:47] = cubeCopy[51:53]
+
+    cube = ''.join(cube)
+def Bp():
+    global cube
+    cube = list(cube)
+    cubeCopy = cube.copy()
+
+    cube[2:5] = cubeCopy[47:50]
+    cube[38:41] = cubeCopy[2:5]
+    cube[11:14] = cubeCopy[38:41]
+    cube[47:50] = cubeCopy[11:14]
+
+    for i in range(18,24): cube[i] = cubeCopy[i+2]
+    cube[24:26] = cubeCopy[18:20]
+
+    cube = ''.join(cube)
+
+moves = {"R": R ,"L" :L ,"U" :U ,"D" :D ,"F" :F ,"B" :B ,
+         "R'":Rp,"L'":Lp,"U'":Up,"D'":Dp,"F'":Fp,"B'":Bp}
+
+
+
+def FormatterSequence(sequence):
+    if sequence == "o" : return
+    sequence = sequence.upper()#;print("sequence :",sequence)
+    return (lambda res=[], i=0: (
+        (lambda f: f(f, res, i))(
+            lambda f, r, i: r if i >= len(sequence) else (
+                r.extend([sequence[i], sequence[i]]) or f(f, r, i+2) if i+1 < len(sequence) and sequence[i+1] == '2' else
+                r.extend([sequence[i]+'\'']) or f(f, r, i+2) if i+1 < len(sequence) and sequence[i+1] == "'" else
+                r.append(sequence[i]) or f(f, r, i+1)
+            )
+        )
+    ))()
+def Sequence(sequence:str):
+    sequence = FormatterSequence(sequence)
+    global cube
     for move in sequence:
-        match move:
-            case "R":R(1)
-            case "L":L(1)
-            case "U":U(1)
-            case "D":D(1)
-            case "F":F(1)
-            case "B":B(1)
-            case "R'":R(-1)
-            case "L'":L(-1)
-            case "U'":U(-1)
-            case "D'":D(-1)
-            case "F'":F(-1)
-            case "B'":B(-1)
+        moves[move]()
 
-def RechercheCroix(cube=cube,solveSequence=solveSequence):
-    colorLeft = ['b','r','o','g']; c=0
-    while colorLeft != []:
-        c +=1 # Securité
-        for face in cube.keys():
-            for facet in range(1,8,2):
-                if cube[face][facet] == 'w':                            # On vient de trouver une vignette blanche
-                    #print(face,facet)
+def RechercheCroix(scramble):
+    Sequence(scramble)
+    global cube
+    c = 0
+    dico_cubes = {cube:""} # cube state : "sequence to get to it"
+    cube_a_traiter = [cube]
+    while cube_a_traiter != []:
+        for cube_precedent in cube_a_traiter:
+            cube_a_traiter.remove(cube)
+            for move in moves :
+                cube = cube_precedent
+                moves[move]()
+                sequence = dico_cubes[cube_precedent] + move
+                if [cube[i] for i in [1,3,5,7]] == ["u","u","u","u"]: # on peut généraliser pour tous les coté pour la version color neutral
+                    print("Cross sequence found after",c,"attempts :",sequence)# in",len(sequence),"moves"
+                    cube = cube_o
+                    return sequence
+                if dico_cubes.get(cube) == None:
+                    dico_cubes[cube] = sequence
+                    cube_a_traiter.append(cube)
+                elif len(dico_cubes[cube]) > len(sequence) :
+                    dico_cubes[cube] = sequence
+                c+=1
+                
 
-                    faceLinked,facetLinked = linkEdges[face][facet]
-                    colorLinked = cube[faceLinked][facetLinked]         # Couleur de l'arrête
-                    #print(faceLinked,facetLinked,colorLinked)
-                    if colorLinked not in colorLeft: break              # Déjà faite ?
-
-                    faceColor = cube[face][8]                           # Face avec le côté blanc de l'arrête
-                    faceLinkedColor = cube[faceLinked][8]               # Face avec la couleur de l'arrête
-                    #print(faceLinkedColor,linkColorFace[faceLinkedColor])
-
-                    if colorLinked == faceLinkedColor :
-                        if 'y' == faceColor:
-                            Sequence(faceLinked+"2")
-                            solveSequence += faceLinked+"2"
-                        else :
-                            Sequence(faceLinked)
-                            if cube['u'][linkFaces['u'][faceLinked][0]] == 'w':
-                                solveSequence += faceLinked
-                            else :
-                                Sequence(faceLinked+"2")
-                                solveSequence += faceLinked+"'"
-
-                    if colorLinked == opposedColor[faceLinkedColor] :
-                        Sequence(face+"2" +linkColorFace[colorLinked])
-                        if cube['u'][linkFaces['u'][faceLinked][0]] != 'w':
-                            Sequence(linkColorFace[colorLinked]+"2")
-                            solveSequence += face+"2"+linkColorFace[colorLinked]+"'"+face+"2"
-                        else :
-                            Sequence(face+"2")
-                            solveSequence += face+"2"+linkColorFace[colorLinked]+face+"2"
-                    colorLeft.remove(colorLinked) # On part du pcpe que c'est fini donc attention
-                    break
-        if c > 50 :
-            print('OVERFLOW'); break
-    print(solveSequence)
-                        
-"RL2F2B2"
-"R2L2U2D2F2B2FR2UD2B'DRUD2F2UR2U'F2DL2B2D2L2"
-Sequence("RL2F2B2")
-RechercheCroix()
+                           
 
 
 
-from turtle import *;hideturtle();bgcolor("black");goto(-75,300)
+
 def Square(color :str):
     fillcolor(color[:7])
     begin_fill()
+    down()
     for i in range(4):
         forward(50)
         right(90)
     end_fill()
     forward(50)
-
-    goto(pos()[0]-25,pos()[1]-35)
-    write(color[7:], align="center", font=("Courier", 16, "bold"))
-    goto(pos()[0]+25,pos()[1]+35)
+    up()
 def Face(face :list):
     i = 0
     for _ in range(3):
         for _ in range(3):
-            Square(colors[cube[face][index[i]]])
+            Square(colors[face[index[i]]])
             i+=1
         goto(pos()[0]-150,pos()[1]-50)
 def TracerCube():
-    clear();up();tracer(0);Face("u");Face("f");Face("d");Face("b");goto(-225,150);Face("l");goto(75,150);Face("r");update();done()
+    clear();tracer(0)
 
-TracerCube()
+    goto(-75,+300);Face(cube[0:9])
+    goto(-75,-0)  ;Face(cube[9:18])
+    goto(+75,150) ;Face(cube[18:27])
+    goto(-225,150);Face(cube[27:36])
+    goto(-75,150) ;Face(cube[36:45])
+    goto(-75,-150);Face(cube[45:54])
+    
+    update()
+def LireSequence(sequence):
+    sequence = FormatterSequence(sequence)
+    global cube
+    TracerCube()
+    sleep(1.5)
+    for move in sequence:
+        moves[move]()
+        TracerCube()
+        sleep(.5)
+
+scramble = "L'B'D2B2L2D2FD2F'U2L2B'U2R'F'DU'LRU"
+"R2L2U2D2F2B2FR2UD2B'DRUD2F2UR2U'F2DL2B2D2L2"
+"RL2F2B2"
+
+print("Scramble :",scramble)
+# sequenceCroix = RechercheCroix(scramble)
+LireSequence(scramble)
+# LireSequence(sequenceCroix)
+LireSequence("BBBL'FRL")
+done()
